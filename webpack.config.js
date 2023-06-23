65dd8605204e2dbf1867db0561eac352c8f6274e
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2023-06-23 13:19:50 +0400

    feat: inits app, configures webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './public/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
+    },
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/i,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: '/node-modules',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
